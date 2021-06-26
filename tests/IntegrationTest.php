f10d0d3a6b6bd5ade582e27de3059f8363f8df21
mpyw <ryosuke_i_628@yahoo.co.jp>
2021-06-27 01:09:30 +0900

    test: 💍 More detailed tests

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -2,6 +2,7 @@
 
 namespace Mpyw\LaravelPdoEmulationControl\Tests;
 
+use Closure;
 use Illuminate\Support\Facades\DB;
 use Mpyw\LaravelPdoEmulationControl\ConnectionServiceProvider;
 use Orchestra\Testbench\TestCase;
@@ -29,6 +30,38 @@ class IntegrationTest extends TestCase
     }
 
     /**
+     * @param  string        $property
+     * @return \Closure|\PDO
+     */
+    protected function getConnectionPropertyValue(string $property)
+    {
+        $db = DB::connection();
+        $rp = new \ReflectionProperty($db, $property);
+        $rp->setAccessible(true);
+        return $rp->getValue($db);
+    }
+
+    protected function assertPdoResolved(): void
+    {
+        $this->assertInstanceOf(PDO::class, $this->getConnectionPropertyValue('pdo'));
+    }
+
+    protected function assertPdoNotResolved(): void
+    {
+        $this->assertInstanceOf(Closure::class, $this->getConnectionPropertyValue('readPdo'));
+    }
+
+    protected function assertReadPdoResolved(): void
+    {
+        $this->assertInstanceOf(PDO::class, $this->getConnectionPropertyValue('pdo'));
+    }
+
+    protected function assertReadPdoNotResolved(): void
+    {
+        $this->assertInstanceOf(Closure::class, $this->getConnectionPropertyValue('readPdo'));
+    }
+
+    /**
      * @param  \Illuminate\Foundation\Application $app
      * @return array
      */
@@ -41,41 +74,90 @@ class IntegrationTest extends TestCase
 
     public function testEagerEmulated(): void
     {
+        $this->assertPdoNotResolved();
+        $this->assertReadPdoNotResolved();
+
         $this->assertSame(0, DB::getReadPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
         $this->assertSame(0, DB::getPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
 
+        $this->assertPdoResolved();
+        $this->assertReadPdoResolved();
+
         $result = DB::emulated(function ($n) {
+            $this->assertPdoResolved();
+            $this->assertReadPdoResolved();
+
             $this->assertSame(1, DB::getReadPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
             $this->assertSame(1, DB::getPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
+
+            $this->assertPdoResolved();
+            $this->assertReadPdoResolved();
+
             return ++$n;
         }, 1);
 
+        $this->assertPdoResolved();
+        $this->assertReadPdoResolved();
+
         $this->assertSame(0, DB::getReadPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
         $this->assertSame(0, DB::getPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
+
+        $this->assertPdoResolved();
+        $this->assertReadPdoResolved();
+
         $this->assertSame(2, $result);
     }
 
     public function testLazyEmulated(): void
     {
+        $this->assertPdoNotResolved();
+        $this->assertReadPdoNotResolved();
+
         $result = DB::emulated(function ($n) {
+            $this->assertPdoNotResolved();
+            $this->assertReadPdoNotResolved();
+
             $this->assertSame(1, DB::getReadPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
             $this->assertSame(1, DB::getPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
+
+            $this->assertPdoResolved();
+            $this->assertReadPdoResolved();
             return ++$n;
         }, 1);
 
+        $this->assertPdoResolved();
+        $this->assertReadPdoResolved();
+
         $this->assertSame(0, DB::getReadPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
         $this->assertSame(0, DB::getPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
+
+        $this->assertPdoResolved();
+        $this->assertReadPdoResolved();
+
         $this->assertSame(2, $result);
     }
 
     public function testCancelEmulated(): void
     {
+        $this->assertPdoNotResolved();
+        $this->assertReadPdoNotResolved();
+
         $result = DB::emulated(function ($n) {
+            $this->assertPdoNotResolved();
+            $this->assertReadPdoNotResolved();
+
             return ++$n;
         }, 1);
 
+        $this->assertPdoNotResolved();
+        $this->assertReadPdoNotResolved();
+
         $this->assertSame(0, DB::getReadPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
         $this->assertSame(0, DB::getPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
+
+        $this->assertPdoResolved();
+        $this->assertReadPdoResolved();
+
         $this->assertSame(2, $result);
     }
 
@@ -83,17 +165,37 @@ class IntegrationTest extends TestCase
     {
         config(['database.connections.mysql.options' => [PDO::ATTR_EMULATE_PREPARES => true]]);
 
+        $this->assertPdoNotResolved();
+        $this->assertReadPdoNotResolved();
+
         $this->assertSame(1, DB::getReadPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
         $this->assertSame(1, DB::getPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
 
+        $this->assertPdoResolved();
+        $this->assertReadPdoResolved();
+
         $result = DB::native(function ($n) {
+            $this->assertPdoResolved();
+            $this->assertReadPdoResolved();
+
             $this->assertSame(0, DB::getReadPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
             $this->assertSame(0, DB::getPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
+
+            $this->assertPdoResolved();
+            $this->assertReadPdoResolved();
+
             return ++$n;
         }, 1);
 
+        $this->assertPdoResolved();
+        $this->assertReadPdoResolved();
+
         $this->assertSame(1, DB::getReadPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
         $this->assertSame(1, DB::getPdo()->getAttribute(PDO::ATTR_EMULATE_PREPARES));
+
+        $this->assertPdoResolved();
+        $this->assertReadPdoResolved();
+
         $this->assertSame(2, $result);
     }
 }
